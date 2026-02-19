e02e8459ded72db49272b075e9ec33b85650dc23
fubangkh <8779417@asia-fort.com>
2026-02-19 17:17:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -467,21 +467,22 @@ if pwd == ADMIN_PWD:
         column_config={
             "录入编号": st.column_config.TextColumn("录入编号", width="small"),
             "摘要": st.column_config.TextColumn("摘要", width="large"),
-            "客户/项目信息": st.column_config.TextColumn("客户/项目信息", width="medium", alignment="left"),
-            "结算账户": st.column_config.TextColumn("结算账户", width="medium", alignment="left"),
-            "审批/发票单号": st.column_config.TextColumn("审批/发票单号", width="medium", alignment="left"),
-            "资金性质": st.column_config.TextColumn("资金性质", width="small", alignment="center"),
+            "客户/项目信息": st.column_config.TextColumn("客户/项目信息", width="medium"),
+            "结算账户": st.column_config.TextColumn("结算账户", width="medium"),
+            "审批/发票单号": st.column_config.TextColumn("审批/发票单号", width="medium"),
+            "资金性质": st.column_config.TextColumn("资金性质", width="small"),
             "实际金额": st.column_config.NumberColumn("流水原数", format="%.2f", width="small"),
-            "实际币种": st.column_config.TextColumn("实际币种", width="small", alignment="center"),
+            "实际币种": st.column_config.Column("实际币种", width="small", alignment="center"),
             "收入": st.column_config.NumberColumn("收入(USD)", format="$%.2f"),
             "支出": st.column_config.NumberColumn("支出(USD)", format="$%.2f"),
             "余额": st.column_config.NumberColumn("余额(USD)", format="$%.2f"),
-            "经手人": st.column_config.TextColumn("经手人", width="small", alignment="left"),
-            "备注": st.column_config.TextColumn("备注", width="medium", alignment="center"),
+            "经手人": st.column_config.TextColumn("经手人", width="small"),
+            "备注": st.column_config.TextColumn("备注", width="medium"),
         }
     )
 else:
     st.info("请输入密码解锁系统")
+
 
 
 

269b17172e5998461dfd57a878519b9c69cb3166
fubangkh <8779417@asia-fort.com>
2026-02-19 16:41:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -466,16 +466,17 @@ if pwd == ADMIN_PWD:
         hide_index=True,
         column_config={
             "实际金额": st.column_config.NumberColumn("流水原数", format="%.2f", width="small"),
-            "实际币种": st.column_config.TextColumn("币种单位", width="small"),
+            "实际币种": st.column_config.TextColumn("实际币种", width="small"),
             "收入": st.column_config.NumberColumn("收入(USD)", format="$%.2f"),
             "支出": st.column_config.NumberColumn("支出(USD)", format="$%.2f"),
-            "余额": st.column_config.Column("余额", width="small"),
+            "余额": st.column_config.NumberColumn("余额(USD)", format="$%.2f"),
             "摘要": st.column_config.TextColumn("摘要", width="large"),
             "录入编号": st.column_config.TextColumn("录入编号", width="small")
         }
     )
 else:
     st.info("请输入密码解锁系统")
+
 
 
 

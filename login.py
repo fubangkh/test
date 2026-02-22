047aef25c9c67c9bee13379e4510adfb587e0bb3
fubangkh <8779417@asia-fort.com>
2026-02-22 10:07:18 +0700

    Update login.py to 优化了富邦日记账和FB字体大小

--- login.py
+++ login.py
@@ -94,10 +94,10 @@ def show_login_page():
         st.markdown(f'<div class="header-box"><div class="logo-circle">FB</div><h1 class="title-text">富邦日记账</h1></div>', unsafe_allow_html=True)
 
         st.markdown(f'<div class="label-with-icon"><img src="{user_svg}"> 账号</div>', unsafe_allow_html=True)
-        u = st.text_input("账号", placeholder="请输入账号", key="user", label_visibility="collapsed")
+        u = st.text_input("账号", placeholder="请输入账号，测试账号123", key="user", label_visibility="collapsed")
         
         st.markdown(f'<div class="label-with-icon" style="margin-top:10px;"><img src="{lock_svg}"> 密码</div>', unsafe_allow_html=True)
-        p = st.text_input("密码", placeholder="请输入密码", type="password", key="pwd", label_visibility="collapsed")
+        p = st.text_input("密码", placeholder="请输入密码，测试密码123", type="password", key="pwd", label_visibility="collapsed")
 
         st.checkbox("记住我", value=True)
 
